3ff1216b39717ae2a364f1bb7c4597bf5a6b0fdc
aabood.17 <abod.83.2003@gmail.com>
2024-08-30 01:45:28 +0300

    updates

--- resources/views/home/side.blade.php
+++ resources/views/home/side.blade.php
@@ -35,10 +35,10 @@
                     </div>
                     <div class="hero__item set-bg" data-setbg="img/hero/banner.jpg">
                         <div class="hero__text">
-                            <span>FRUIT FRESH</span>
-                            <h2>Vegetable <br />100% Organic</h2>
+                            <span>High Quality</span>
+                            <h2>All PRODUCTS <br />100% Original</h2>
                             <p>Free Pickup and Delivery Available</p>
-                            <a href="#" class="primary-btn">SHOP NOW</a>
+                            <a href="{{ route('shop') }}" class="primary-btn">SHOP NOW</a>
                         </div>
                     </div>
                 </div>
