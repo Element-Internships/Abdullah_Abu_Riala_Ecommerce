d2b5197cab674796cb438f890f28d71a2f1646dd
aabood.17 <abod.83.2003@gmail.com>
2024-08-30 03:01:23 +0300

    updates

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -1,9 +1,4 @@
-<meta charset="UTF-8">
-    <meta name="description" content="Ecommerce website for BU Internship 2024">
-    <meta name="keywords" content="Ecommerce, Bethlehem University, Frontend, Backend, Laravel, Internship">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ecommerce BU Internship</title>
+
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
