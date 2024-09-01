9b239141a515e2e140f30016cb8f0bf9b3ba0d65
aabood17 <abod.83.2003@gmail.com>
2024-09-01 19:33:49 +0300

    Search

--- resources/views/home/script.blade.php
+++ resources/views/home/script.blade.php
@@ -9,6 +9,7 @@
 <script src="{{ asset('home/js/main.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <!-- Js Plugins -->
+ 
 <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.nice-select.min.js"></script>
